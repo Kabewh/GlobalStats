2571c3dd6289dd07adef2323c4b2604460fa84df
Kabewh <daniele_viola@yahoo.com>
2023-10-06 17:00:40 +0300

    not rendering the first time issue

--- root/reactapp/src/components/PieChart.jsx
+++ root/reactapp/src/components/PieChart.jsx
@@ -41,61 +41,56 @@ const PieChart = () => {
         "India", "Japan", "Mexico", "Brazil", "Bangladesh", "Nigeria", "Indonesia", "Pakistan", "China"
     ]
 
-    // const [birthsByCountry, setBirthsByCountry] = useState([
-    //     {
-    //         country: '',
-    //         births: 0,
-    //         estimatedBirths: 0,
-    //     }
-    // ])
-
-    // const updateCountries = () => {
-    //     const UpdatedBirthsByCountry = countries.map((country) => {
-    //         return (
-    //             {
-    //                 country: country,
-    //                 births: 0,
-    //                 estimatedBirths: 0
-    //             }
-    //         )
-    //     }
-    //     )
-    //     setBirthsByCountry(UpdatedBirthsByCountry)
-    //     console.log(birthsByCountry)
-    // }
     const [birthsWorld, setBirthsWorld] = useState([]);
     const [estimatedBirthsWorld, setEstimatedBirthsWorld] = useState([]);
 
     useEffect(() => {
-        async function fetchYoungerOlderWorld() {
+        fetchYoungerOlderWorld();
+        sharedBirthsWorld()
+    }, [estimatedBirthsWorld])
+
+    async function fetchYoungerOlderWorld() {
+        const promises = countries.map(async (country) => {
+            const response = await fetch(LOCALHOST + `youngerOlderInfo/${country}/2023/`)
+            const jsonData = await response.json()
+            return { country, births: jsonData }
+        })
+        const results = await Promise.all(promises);
+        setBirthsWorld(results)
+    }
+
+    async function sharedBirthsWorld() {
+        if (countryChoice && yearChoice) {
             const promises = countries.map(async (country) => {
-                const response = await fetch(LOCALHOST + `youngerOlderInfo/${country}/2023/`)
+                const response = await fetch(LOCALHOST + `population/${country}/2023/`)
                 const jsonData = await response.json()
-                return { country, births: jsonData }
+                return { country, population: jsonData }
             })
             const results = await Promise.all(promises);
-            setBirthsWorld(results.reverse())
-            console.log("births world: ", birthsWorld);
-        }
 
-        fetchYoungerOlderWorld();
-    }, [])
+            const population = results.forEach((country) => {
+                const births = birthsWorld.find((births) => births.country === country.country);
 
-    // console.log("births world: ", birthsWorld)
-    // async function sharedBirthsWorld() {
-    //     if (countryChoice && yearChoice) {
-    //         const response = await fetch(LOCALHOST + "population/World/2023")
-    //         const jsonData = await response.json()
-    //         const population = jsonData;
-    //         const avgDailyBirthRate = (birthsWorld / population) * 1000
-    //         const avgBirthRate = (avgDailyBirthRate / 1000) * 365
-    //         const proportionBirths = 1 / 365
-    //         const estimatedBirths = avgBirthRate * proportionBirths / 100 * population
-    //         setEstimatedBirthsWorld(estimatedBirths)
-    //         console.log("estimated births: ", estimatedBirths)
-    //         console.log("births world: ", birthsWorld)
-    //     }
-    // }
+                if (births) {
+                    const avgDailyBirthRate = (births.births / country.population) * 1000;
+                    const avgBirthRate = (avgDailyBirthRate / 1000) * 365;
+                    const proportionBirths = 1 / 365;
+                    const estimatedBirths = Math.round((avgBirthRate * proportionBirths * country.population) / 100);
+
+                    if (estimatedBirthsWorld.length < 9) {
+                        setEstimatedBirthsWorld(prevState => [
+                            ...prevState,
+                            {
+                                country: country.country,
+                                births: estimatedBirths.toString(),
+                            }
+                        ]);
+                    }
+                }
+            });
+        };
+    }
+
 
     const options = {
         type: 'doughnut',
@@ -131,13 +126,12 @@ const PieChart = () => {
     };
 
     const renderedData = () => {
-        if (birthsWorld.length > 0) {
+        if (estimatedBirthsWorld.length > 0) {
             return (
-                [birthsWorld[0].births, birthsWorld[1].births, birthsWorld[2].births, birthsWorld[3].births, birthsWorld[4].births, birthsWorld[5].births, birthsWorld[6].births, birthsWorld[7].births, birthsWorld[8].births]
+                [estimatedBirthsWorld[0].births, estimatedBirthsWorld[1].births, estimatedBirthsWorld[2].births, estimatedBirthsWorld[3].births, estimatedBirthsWorld[4].births, estimatedBirthsWorld[5].births, estimatedBirthsWorld[6].births, estimatedBirthsWorld[7].births, estimatedBirthsWorld[8].births]
             )
         }
     }
-    // console.log(birthsWorld)
 
     const data = {
         datasets: [{
