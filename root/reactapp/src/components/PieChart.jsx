557125f79009903d44237c5931e159ed3ca737a5
Daniele Viola <daniele.viola02@e-uvt.ro>
2023-10-17 14:26:35 +0300

    solved not rendering the first time issue

--- root/reactapp/src/components/PieChart.jsx
+++ root/reactapp/src/components/PieChart.jsx
@@ -1,158 +1,184 @@
-import React, { useEffect, useState } from 'react'
-import "chartjs-plugin-datalabels"
+import React, { useEffect, useState } from "react";
+import "chartjs-plugin-datalabels";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Filler,
-    Legend,
-    ArcElement,
-} from 'chart.js';
-import { Pie } from 'react-chartjs-2';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+  ArcElement,
+} from "chart.js";
+import { Pie } from "react-chartjs-2";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
-
-const total_deaths = 69248000;
-const LOCALHOST = "http://localhost:8000/"
-const countryChoice = true
-const yearChoice = true
+const LOCALHOST = "http://localhost:8000/";
 
 ChartJS.register(
-    ArcElement,
-    ChartDataLabels,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Filler,
-    Legend
-)
-
+  ArcElement,
+  ChartDataLabels,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
 
 const PieChart = () => {
+  const countries = [
+    "India",
+    "Japan",
+    "Mexico",
+    "Brazil",
+    "Bangladesh",
+    "Nigeria",
+    "Indonesia",
+    "Pakistan",
+    "China",
+  ];
 
-    const countries = [
-        "India", "Japan", "Mexico", "Brazil", "Bangladesh", "Nigeria", "Indonesia", "Pakistan", "China"
-    ]
+  const [birthsWorld, setBirthsWorld] = useState([]);
+  const [estimatedBirthsWorld, setEstimatedBirthsWorld] = useState([]);
 
-    const [birthsWorld, setBirthsWorld] = useState([]);
-    const [estimatedBirthsWorld, setEstimatedBirthsWorld] = useState([]);
+  useEffect(() => {
+    fetchYoungerOlderWorld();
+  }, []);
 
-    useEffect(() => {
-        fetchYoungerOlderWorld();
-        sharedBirthsWorld()
-    }, [estimatedBirthsWorld])
+  async function fetchYoungerOlderWorld() {
+    const promises = countries.map(async (country) => {
+      const response = await fetch(
+        LOCALHOST + `youngerOlderInfo/${country}/2023/`
+      );
+      const jsonData = await response.json();
+      return { country, births: jsonData };
+    });
+    const results = await Promise.all(promises);
+    setBirthsWorld(results);
+  }
 
-    async function fetchYoungerOlderWorld() {
-        const promises = countries.map(async (country) => {
-            const response = await fetch(LOCALHOST + `youngerOlderInfo/${country}/2023/`)
-            const jsonData = await response.json()
-            return { country, births: jsonData }
-        })
-        const results = await Promise.all(promises);
-        setBirthsWorld(results)
+  // async function sharedBirthsWorld() {
+  //   if (countryChoice && yearChoice) {
+  //     const promises = countries.map(async (country) => {
+  //       const response = await fetch(LOCALHOST + `population/${country}/2023/`);
+  //       const jsonData = await response.json();
+  //       return { country, population: jsonData };
+  //     });
+  //     const results = await Promise.all(promises);
+
+  //     const population = results.forEach((country) => {
+  //       const births = birthsWorld.find(
+  //         (births) => births.country === country.country
+  //       );
+
+  //       if (births) {
+  //         const avgDailyBirthRate = (births.births / country.population) * 1000;
+  //         const avgBirthRate = (avgDailyBirthRate / 1000) * 365;
+  //         const proportionBirths = 1 / 365;
+  //         const estimatedBirths = Math.round(
+  //           (avgBirthRate * proportionBirths * country.population) / 100
+  //         );
+
+  //         if (estimatedBirthsWorld.length < 9) {
+  //           setEstimatedBirthsWorld((prevState) => [
+  //             ...prevState,
+  //             {
+  //               country: country.country,
+  //               births: estimatedBirths.toString(),
+  //             },
+  //           ]);
+  //         }
+  //       }
+  //     });
+  //   }
+  // }
+
+  const options = {
+    type: "doughnut",
+    hitRadius: 5,
+    responsive: true,
+    plugins: {
+      datalabels: {
+        display: true,
+        color: "#222",
+        font: {
+          weight: "bold",
+        },
+        formatter: function (value, context) {
+          return context.dataset.labels[context.dataIndex];
+        },
+      },
+      collision: {
+        enabled: false,
+      },
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: true,
+        callbacks: {
+          label: function (context) {
+            const intcontext = parseInt(
+              context.dataset.data[context.dataIndex]
+            ).toLocaleString();
+            return `Births ${intcontext}`;
+          },
+        },
+      },
+    },
+  };
+
+  const renderedData = () => {
+    if (birthsWorld.length > 0) {
+      console.log(birthsWorld);
+      return birthsWorld.map((births) => births.births);
     }
+  };
 
-    async function sharedBirthsWorld() {
-        if (countryChoice && yearChoice) {
-            const promises = countries.map(async (country) => {
-                const response = await fetch(LOCALHOST + `population/${country}/2023/`)
-                const jsonData = await response.json()
-                return { country, population: jsonData }
-            })
-            const results = await Promise.all(promises);
+  const data = {
+    datasets: [
+      {
+        labels: [
+          "India",
+          "Japan",
+          "Mexico",
+          "Brazil",
+          "Bangladesh",
+          "Nigeria",
+          "Indonesia",
+          "Pakistan",
+          "China",
+        ],
+        data: renderedData(),
+        backgroundColor: [
+          "#E6E6FA",
+          "#98FF98",
+          "#FADADD",
+          "#87CEEB",
+          "#C0C0C0",
+          "#FFDAB9",
+          "#FFFFE0",
+          "#C8A2C8",
+          "#98FB98",
+          "#F8FD",
+        ],
+      },
+    ],
+  };
 
-            const population = results.forEach((country) => {
-                const births = birthsWorld.find((births) => births.country === country.country);
+  return (
+    <>
+      <div className="pie">
+        <h4>Top 10 Countries by Birthdays</h4>
+        {data ? <Pie options={options} data={data} /> : <h2>Loading...</h2>}
+      </div>
+    </>
+  );
+};
 
-                if (births) {
-                    const avgDailyBirthRate = (births.births / country.population) * 1000;
-                    const avgBirthRate = (avgDailyBirthRate / 1000) * 365;
-                    const proportionBirths = 1 / 365;
-                    const estimatedBirths = Math.round((avgBirthRate * proportionBirths * country.population) / 100);
-
-                    if (estimatedBirthsWorld.length < 9) {
-                        setEstimatedBirthsWorld(prevState => [
-                            ...prevState,
-                            {
-                                country: country.country,
-                                births: estimatedBirths.toString(),
-                            }
-                        ]);
-                    }
-                }
-            });
-        };
-    }
-
-
-    const options = {
-        type: 'doughnut',
-        hitRadius: 5,
-        responsive: true,
-        plugins: {
-            datalabels: {
-                display: true,
-                color: "#222",
-                font: {
-                    weight: "bold"
-                },
-                formatter: function (value, context) {
-                    return context.dataset.labels[context.dataIndex];
-                }
-            },
-            collision: {
-                enabled: false
-            },
-            legend: {
-                display: false,
-            },
-            tooltip: {
-                enabled: true,
-                callbacks: {
-                    label: function (context) {
-                        const intcontext = parseInt(context.dataset.data[context.dataIndex]).toLocaleString()
-                        return `Births ${intcontext}`;
-                    }
-                }
-            },
-        }
-    };
-
-    const renderedData = () => {
-        if (estimatedBirthsWorld.length > 0) {
-            return (
-                [estimatedBirthsWorld[0].births, estimatedBirthsWorld[1].births, estimatedBirthsWorld[2].births, estimatedBirthsWorld[3].births, estimatedBirthsWorld[4].births, estimatedBirthsWorld[5].births, estimatedBirthsWorld[6].births, estimatedBirthsWorld[7].births, estimatedBirthsWorld[8].births]
-            )
-        }
-    }
-
-    const data = {
-        datasets: [{
-            labels: ['India', 'Japan', 'Mexico', 'Brazil', 'Bangladesh', 'Nigeria', 'Indonesia', 'Pakistan', 'China'],
-            data: renderedData(),
-            backgroundColor: ['#E6E6FA', '#98FF98', '#FADADD', '#87CEEB', '#C0C0C0', '#FFDAB9', '#FFFFE0', '#C8A2C8', '#98FB98', '#F8FD'],
-        }
-        ]
-
-    };
-
-    return (
-        <>
-            <div className='pie'>
-                <h4>Top 10 Countries by Birthdays</h4>
-                {data ? <Pie options={options} data={data} /> : console.log("data not loaded")}
-
-            </div>
-        </>
-
-    )
-}
-
-export default PieChart
+export default PieChart;
--- root/reactapp/src/components/WorldClock.jsx
+++ root/reactapp/src/components/WorldClock.jsx
@@ -1,51 +1,66 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
-const LOCALHOST = "http://localhost:8000/"
+const LOCALHOST = "http://localhost:8000/";
 
 const WorldClock = () => {
-    const [simulatedPopulation, setSimulatedPopulation] = useState(0);
-    const [divElements, setDivElements] = useState([]);
+  const [simulatedPopulation, setSimulatedPopulation] = useState(0);
+  const [divElements, setDivElements] = useState([]);
 
-    useEffect(() => {
-        fetchPopulation()
-        calculatePopulation()
-    }, []);
+  useEffect(() => {
+    fetchPopulation();
+    calculatePopulation();
+  }, []);
 
+  async function fetchPopulation() {
+    const response = await fetch(
+      LOCALHOST + "population/" + "World" + "/" + "2023" + "/"
+    );
+    const jsonData = await response.json();
+    setSimulatedPopulation(jsonData);
+  }
 
-    async function fetchPopulation() {
-        const response = await fetch(LOCALHOST + "population/" + "World" + "/" + "2023" + "/")
-        const jsonData = await response.json()
-        setSimulatedPopulation(jsonData)
-    }
+  async function calculatePopulation() {
+    const response = await fetch(LOCALHOST + "population/World/2023");
+    // const jsonData = await response.json()
+    // const popgrowthrate = 0.0108 // 18.5 crude birth rate - 7.7 crude death rate / 1000  rata neta a cresterii populatiei per 1000 locuitori
+    // const secondBirthRate = 8_000_000_000 * 0.0108 / (365 * 24 * 60 * 60) // rata nasterii pe secunda
+    const interval = setInterval(() => {
+      setSimulatedPopulation((data) => data + 3);
+      addDivElement();
+    }, 1000);
+    return () => clearInterval(interval);
+  }
 
-    async function calculatePopulation() {
-        const response = await fetch(LOCALHOST + "population/World/2023")
-        const jsonData = await response.json()
-        const popgrowthrate = 0.0108 // 18.5 crude birth rate - 7.7 crude death rate / 1000  rata neta a cresterii populatiei per 1000 locuitori
-        const secondBirthRate = 8_000_000_000 * 0.0108 / (365 * 24 * 60 * 60) // rata nasterii pe secunda 
-        const interval = setInterval(() => {
-            setSimulatedPopulation(data => data + 3);
-            addDivElement()
-        }, 1000);
-        return () => clearInterval(interval);
-    }
+  const addDivElement = () => {
+    setDivElements((prevDivs) => [
+      ...prevDivs,
+      <div key={prevDivs.length} class="babyGenerator">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          className="up"
+        >
+          <path d="M152 88a72 72 0 1 1 144 0A72 72 0 1 1 152 88zM39.7 144.5c13-17.9 38-21.8 55.9-8.8L131.8 162c26.8 19.5 59.1 30 92.2 30s65.4-10.5 92.2-30l36.2-26.4c17.9-13 42.9-9 55.9 8.8s9 42.9-8.8 55.9l-36.2 26.4c-13.6 9.9-28.1 18.2-43.3 25V288H128V251.7c-15.2-6.7-29.7-15.1-43.3-25L48.5 200.3c-17.9-13-21.8-38-8.8-55.9zm89.8 184.8l60.6 53-26 37.2 24.3 24.3c15.6 15.6 15.6 40.9 0 56.6s-40.9 15.6-56.6 0l-48-48C70 438.6 68.1 417 79.2 401.1l50.2-71.8zm128.5 53l60.6-53 50.2 71.8c11.1 15.9 9.2 37.5-4.5 51.2l-48 48c-15.6 15.6-40.9 15.6-56.6 0s-15.6-40.9 0-56.6L284 419.4l-26-37.2z" />
+        </svg>
+      </div>,
+    ]);
+  };
 
-    const addDivElement = () => {
-        setDivElements(prevDivs => [...prevDivs, <div key={prevDivs.length} class="babyGenerator"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className='up'><path d="M152 88a72 72 0 1 1 144 0A72 72 0 1 1 152 88zM39.7 144.5c13-17.9 38-21.8 55.9-8.8L131.8 162c26.8 19.5 59.1 30 92.2 30s65.4-10.5 92.2-30l36.2-26.4c17.9-13 42.9-9 55.9 8.8s9 42.9-8.8 55.9l-36.2 26.4c-13.6 9.9-28.1 18.2-43.3 25V288H128V251.7c-15.2-6.7-29.7-15.1-43.3-25L48.5 200.3c-17.9-13-21.8-38-8.8-55.9zm89.8 184.8l60.6 53-26 37.2 24.3 24.3c15.6 15.6 15.6 40.9 0 56.6s-40.9 15.6-56.6 0l-48-48C70 438.6 68.1 417 79.2 401.1l50.2-71.8zm128.5 53l60.6-53 50.2 71.8c11.1 15.9 9.2 37.5-4.5 51.2l-48 48c-15.6 15.6-40.9 15.6-56.6 0s-15.6-40.9 0-56.6L284 419.4l-26-37.2z" /></svg></div>]);
+  return (
+    <div className="world-clock">
+      <h3>
+        <p>
+          Current world<br></br>population clock
+        </p>
+        <p className="population-counter">
+          {simulatedPopulation.toLocaleString("en")}
+        </p>
+        <div className="babies"></div>
+        {divElements.map((div) => div).reverse()}
+      </h3>
+    </div>
+  );
+};
 
-    };
-
-    return (
-        <div className="world-clock">
-            <h3>
-                <p>Current world<br></br>population clock</p>
-                <p className="population-counter">{simulatedPopulation.toLocaleString("en")}</p>
-                <div className='babies'>
-                </div>{divElements.map(div => div).reverse()}
-            </h3>
-        </div>
-    )
-}
-
-export default WorldClock
+export default WorldClock;
